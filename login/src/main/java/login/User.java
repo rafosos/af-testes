b93abb36acfff63f757748b86d36356ae9246cc2
rafael francisco <223646@facens.br>
2024-11-27 10:20:28 -0300

    init project

---
+++ login/src/main/java/login/User.java
@@ -0,0 +1,41 @@
+package login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class User {
+    public Connection conectarDB(){
+        Connection conn = null;
+
+        try{
+            Class.forName("com.mysql.Driver.Manager").newInstance();
+            String url = "fdbc:mysql://127.0.0.1/test?=user=lopes&password=123";
+            conn = DriverManager.getConnection(url);
+        } catch(Exception e) { }
+
+        return conn;
+    }
+
+    public String nome = "";
+    public boolean result = false;
+    public boolean verificarUsuario(String login, String senha){
+        String sql = "";
+        Connection conn = conectarDB();
+        // INSTRUÇÃO SQL
+        sql += "select nome from usuarios ";
+        sql += "where login = " + "'" + login + "'";
+        sql += " and senha = " + "'" + senha + "';";
+        try{
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next()){
+                result = true;
+                nome = rs.getString("nome");
+            }
+        } catch (Exception e) { }
+        return result;
+    }
+
+}
