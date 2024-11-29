0fe9b7857084ac7ca98a8e2527fe0bcb41c5ee40
rafael francisco <223646@facens.br>
2024-11-29 16:21:13 -0300

    etapa-4

--- login/src/main/java/login/User.java
+++ login/src/main/java/login/User.java
@@ -5,37 +5,58 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+/** 
+ * Essa classe contém métodos para conectar com o banco de dados e verificar a existência de um usuário. 
+*/
 public class User {
+    /**
+     * Conecta com o banco de dados utilizando connection string padrão. Não retorna exceções.
+     * @return java.sql.Connection
+     */
     public Connection conectarDB(){
-        Connection conn = null;                                                     //1
+        Connection conn = null;
 
         try{
-            Class.forName("com.mysql.Driver.Manager").newInstance();                //1
-            String url = "fdbc:mysql://127.0.0.1/test?=user=lopes&password=123";    //1
-            conn = DriverManager.getConnection(url);                                //1
-        } catch(Exception e) { }                                                    //2
+            Class.forName("com.mysql.Driver.Manager").newInstance();
+            String url = "fdbc:mysql://127.0.0.1/test?=user=lopes&password=123";
+            conn = DriverManager.getConnection(url);
+        } catch(Exception e) { }
 
-        return conn;                                                                //3
+        return conn;
     }
 
+    /**
+     * Nome do usuário encontrado no banco de dados, "" quando não encontrado.
+     */
     public String nome = "";
+
+    /**
+     * true quando a consulta no banco de dados encontra um usuário compatível, false quando não encontrado ou não pesquisado.
+     */
     public boolean result = false;
 
+
+    /**
+     * Verifica se o login e senha informados são compatíveis com algum usuário no banco de dados.
+     * @param login
+     * @param senha
+     * @return true ou false, true para usuário encontrado, false para não encontrado ou falha na pesquisa. 
+     */
     public boolean verificarUsuario(String login, String senha){
-        String sql = "";                                                            //4
-        Connection conn = conectarDB();                                             //4
+        String sql = "";
+        Connection conn = conectarDB();
         // INSTRUÇÃO SQL
-        sql += "select nome from usuarios ";                                        //5
-        sql += "where login = " + "'" + login + "'";                                //5
-        sql += " and senha = " + "'" + senha + "';";                                //5
+        sql += "select nome from usuarios ";
+        sql += "where login = " + "'" + login + "'";
+        sql += " and senha = " + "'" + senha + "';";
         try{
-            Statement st = conn.createStatement();                                  //5
-            ResultSet rs = st.executeQuery(sql);                                    //5
-            if (rs.next()){                                                         //6
-                result = true;                                                      //7
-                nome = rs.getString("nome");                                        //7
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next()){
+                result = true;
+                nome = rs.getString("nome");
             }
-        } catch (Exception e) { }                                                   //8
-        return result;                                                              //9
+        } catch (Exception e) { }
+        return result;
     }
 }
